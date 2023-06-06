8ff2b341620cbb7be52d76c3b21bd51d78b7889a
AnozieChibuike <chibuikeanozie0@gmail.com>
2023-06-06 14:46:44 +0000

    Still getting a hang of git

---
+++ basics/code/c_data.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+  int integer = 1;
+
+  float decimal = 1.7;
+
+  double eight_bytes = 51.7;
+
+  char value = 'a';
+
+  printf("integer : %d\n", integer);
+
+  printf("float : %f\n", decimal);
+
+  printf("double : %lf\n", eight_bytes);
+
+  printf("char : %c\n" , value);
+
+  return 0;
+}
